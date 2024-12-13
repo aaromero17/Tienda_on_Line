93147503ae30b1e627a14d3fbf5127baac014c87
aaromero <aaromero17@gmail.com>
2024-12-13 16:44:20 -0400

    333

--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from gestionPedidos.models import Articulos
+from gestionPedidos.form import FormularioContacto
+
 
 def busqueda_productos(request):
 
@@ -7,5 +10,38 @@ def busqueda_productos(request):
 
 
 def buscar(request):
-    mensaje=f"articulo buscado: {request.GET['prd']}"
+
+    if request.GET['prd']:
+       # mensaje=f"articulo buscado: {request.GET['prd']}"
+
+       
+       
+       
+       producto=request.GET['prd']
+       articulos=Articulos.objects.filter(nombre__icontains=producto)
+       return render(request,"resultado_busqueda.html",{"articulos":articulos,"query":producto})
+    else:
+        mensaje="No has introducido la busqueda"
+    
     return HttpResponse(mensaje)
+
+
+def contacto(request):
+
+    if request.method=="POST":
+        
+        
+        
+         #return render(request,"gracias.html")
+
+        #return render(request,"contacto.html")
+
+        miFormulario=FormularioContacto(request.POST)
+
+        if miFormulario.is_valid():
+            inform=miFormulario.cleaned_data
+            return render(request,"gracias.html")
+    else:
+       miFormulario=FormularioContacto()
+
+    return render(request,"formulario_contacto.html",{"form":miFormulario})
--- tiendaonline/urls.py
+++ tiendaonline/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('busqueda_productos/', views.busqueda_productos),
     path('buscar/', views.buscar),
+    path('contacto/', views.contacto),
+    
    
 ]
